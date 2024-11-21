9a5ab699b98f7f4ba53ba8b253730b2224de5903
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-11-21 05:55:50 -0300

    Feat: update Personagem class to include weapon attribute and methods for getting and setting it

--- src/Personagem.ts
+++ src/Personagem.ts
@@ -1,15 +1,26 @@
 import { Arma } from "./Arm";
 
-class Personagem {
+export default class Personagem {
+    //atributos
     private nome: string;
     private vida: number
     private forca: number;
     private arma: Arma | null;
 
-    constructor(nome: string, vida: number, forca: number) {
+    //construtor
+    constructor(nome: string, vida: number, forca: number, arma: Arma | null) {
         this.nome = nome;
         this.vida = vida;
         this.forca = forca;
-        this.arma = null;
+        this.arma = arma;
+    }
+
+    // metodos
+    getArma(): Arma | null {
+        return this.arma;
+    }
+
+    setArma(arma: Arma): void {
+        this.arma = arma;
     }
 }
